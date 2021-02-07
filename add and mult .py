40012442ec989bd029e310b76feed60195d3cd26
KushalNS <78541160+KushalNS@users.noreply.github.com>
2021-02-07 22:18:24 +0530

    Create add and mult .py

---
+++ add and mult .py
@@ -0,0 +1,8 @@
+first num = int(input("enter first number:"))
+second num = int(input("enter second number:"))
+third num = int(input("enter third number:"))
+result = (first num+second num)*third num
+if value>100:
+       print("Greater than 100")
+else: 
+       print("not greater than 100")
